0bc59edcd817b98e152055aa65668ed5665053c3
TupeaAndrei <59830697+TupeaAndrei@users.noreply.github.com>
2021-04-18 15:33:03 +0300

    Created Operations class.
    Created a utility class that reads the values from a txt file and keeps them in a list of tuple.
    By saving the details for each sign in a txt file this can be easily modified if we ever need to.

---
+++ SeasonsService/SeasonsService/Helper/Operations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeasonsService.Helper
+{
+    public class Operations
+    {
+        private const string FILE_PATH = "./Helper/Textfiles/zodiac.txt";
+        public static List<Tuple<ZodiacObj,ZodiacObj,string>> GetAllZodiacs()
+        {
+            var zodiacList = new List<Tuple<ZodiacObj, ZodiacObj, string>>();
+
+            try
+            {
+                var streamReader = new StreamReader(FILE_PATH);
+                var line = streamReader.ReadLine()?.Split("|");
+                while (line != null)
+                {
+                    zodiacList.Add(new Tuple<ZodiacObj, ZodiacObj, string>(
+                        new ZodiacObj() { Date = line[0] },
+                        new ZodiacObj() { Date = line[1] },
+                        line[2]));
+                    line = streamReader.ReadLine()?.Split("|");
+                }
+                streamReader.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            return zodiacList;
+        }
+    }
+}
