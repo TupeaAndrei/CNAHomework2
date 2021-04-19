f91c398eef8e031d579d682835a00c3e51b5db35
TupeaAndrei <59830697+TupeaAndrei@users.noreply.github.com>
2021-04-19 20:06:35 +0300

    Finished Client Project.
    Finished client project by adding the main class ClientMain.cs , and adding the other files needed for the project.

---
+++ SeasonsService/SeasonsClient/SeasonsClient/Classes/ClientMain.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Net.Client;
+using SeasonsClient.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeasonsClient.Classes
+{
+    class ClientMain
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Zodiac.ZodiacClient(channel);
+
+            var cancellationToken = new CancellationTokenSource(Timeout.Infinite);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                string date = null;
+
+                do
+                {
+                    Console.WriteLine("Enter date: ");
+                    date = Console.ReadLine();
+                } while (!Helper.ValidateDate(date));
+
+                var zodiac = new ZodiacObj() { Date = date != null && date.Trim().Length > 0 ? date : "Invalid!" };
+                var answer = client.AddZodiac(new AddZodiacRequest { ZodiacObj = zodiac });
+                Helper.giveAnswer(answer);
+            }
+        }
+    }
+}
