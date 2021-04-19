093924da89f4b0de75cf2c349b65daea61080cf9
TupeaAndrei <59830697+TupeaAndrei@users.noreply.github.com>
2021-04-19 13:39:10 +0300

    Updated operations.cs class.
    Updated operations.cs class in preparation for the new microservices.

--- SeasonsService/SeasonsService/Helper/Operations.cs
+++ SeasonsService/SeasonsService/Helper/Operations.cs
@@ -9,9 +9,9 @@ namespace SeasonsService.Helper
     public class Operations
     {
         private const string FILE_PATH = "./Helper/Textfiles/zodiac.txt";
-        public static List<Tuple<ZodiacObj,ZodiacObj,string>> GetAllZodiacs()
+        public List<Tuple<string,string,string>> GetAllZodiacs()
         {
-            var zodiacList = new List<Tuple<ZodiacObj, ZodiacObj, string>>();
+            var zodiacList = new List<Tuple<string, string, string>>();
 
             try
             {
@@ -19,9 +19,9 @@ namespace SeasonsService.Helper
                 var line = streamReader.ReadLine()?.Split("|");
                 while (line != null)
                 {
-                    zodiacList.Add(new Tuple<ZodiacObj, ZodiacObj, string>(
-                        new ZodiacObj() { Date = line[0] },
-                        new ZodiacObj() { Date = line[1] },
+                    zodiacList.Add(new Tuple<string, string, string>(
+                        line[0],
+                        line[1],
                         line[2]));
                     line = streamReader.ReadLine()?.Split("|");
                 }
@@ -32,6 +32,94 @@ namespace SeasonsService.Helper
                 Console.WriteLine("Exception: " + e.Message);
             }
             return zodiacList;
+        }
+
+        public List<Tuple<string,string,string>> getSpringZodiac()
+        {
+            var fullList = GetAllZodiacs();
+            var springList = new List<Tuple<string, string, string>>();
+            foreach (var value in fullList)
+            {
+                var month = value.Item1;
+                int monthNum = int.Parse(month.Substring(0, 2));
+                if (monthNum >=3 && monthNum <=5)
+                {
+                    springList.Add(value);
+                }
+            }
+            return springList;
+        }
+
+        public List<Tuple<string, string, string>> getSummerZodiac()
+        {
+            var fullList = GetAllZodiacs();
+            var summerList = new List<Tuple<string, string, string>>();
+            foreach (var value in fullList)
+            {
+                var month = value.Item1;
+                int monthNum = int.Parse(month.Substring(0, 2));
+                if (monthNum >= 6 && monthNum <= 8)
+                {
+                    summerList.Add(value);
+                }
+            }
+            return summerList;
+        }
+
+        public List<Tuple<string, string, string>> getAutumnZodiac()
+        {
+            var fullList = GetAllZodiacs();
+            var autumnList = new List<Tuple<string, string, string>>();
+            foreach (var value in fullList)
+            {
+                var month = value.Item1;
+                int monthNum = int.Parse(month.Substring(0, 2));
+                if (monthNum >= 9 && monthNum <= 11)
+                {
+                    autumnList.Add(value);
+                }
+            }
+            return autumnList;
+        }
+
+        public  List<Tuple<string, string, string>> getWinterZodiac()
+        {
+            var fullList = GetAllZodiacs();
+            var winterList = new List<Tuple<string, string, string>>();
+            foreach (var value in fullList)
+            {
+                var month = value.Item1;
+                int monthNum = int.Parse(month.Substring(0, 2));
+                if (monthNum == 12 || monthNum == 1 || monthNum == 2)
+                {
+                    winterList.Add(value);
+                }
+            }
+            return winterList;
+        }
+
+        public DateTime getBeginDate(Tuple<string,string,string> value)
+        {
+            var begin = value.Item1.Split("/");
+            var beginDay = int.Parse(begin[1]);
+            var beginMonth = int.Parse(begin[0]);
+            var beginDate = new DateTime(2000, beginMonth, beginDay);
+            return beginDate;
+        }
+
+        public DateTime getEndDate(Tuple<string,string,string> value)
+        {
+            var end = value.Item2.Split("/");
+            var endDay = int.Parse(end[1]);
+            var endMonth = int.Parse(end[0]);
+            var endDate = new DateTime(2000, endMonth, endDay);
+            return endDate;
+        }
+
+        public DateTime getCurrentDate(int currentDay,int currentMonth)
+        {
+            var currentDate = new DateTime(2000, currentMonth, currentDay);
+            return currentDate;
         }
     }
 }
